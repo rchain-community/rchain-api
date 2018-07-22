80ab90e513916dea3676705e6ef3c5708feec244
Dan Connolly <dckc@madmode.com>
2018-07-21 21:16:13 -0500

    factor keyPair.js out of gateway/main.js
      - gloss ocap in design notes
      - use def() idiom
      - coin Capper idioms:
        - appFactory
        - state.X throws until init()
      - pass powerful references by { name }

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
-/**
+/** gateway -- RChain / OAuth gateway Capper persistent objects
+
+BUG: clients don't revive when the server restarts.
+
 ISSUE: sessions?
   refresh = require('passport-oauth2-refresh')
   app.use(passport.session());
 
-rho:pubkey:ed25519
-
 ISSUE: indirect to SecretService for CLIENT_SECRET?
-
 */
 const URL = require('url').URL;
 
 const discord = require('passport-discord');
 const github = require('passport-github');
 
+const keyPair = require('./keyPair');
 
-exports.makeGateway = makeGateway;
-/** makeGateway -- construct Capper app for RChain OAuth oracle.
+const def = obj => Object.freeze(obj);  // cf. ocap design note
+
+
+/**
+ * Construct Capper app for RChain OAuth oracle.
+ *
  * app: as from express(), with .use(), .get()
  * passport: as from require('passport'), since it has mutable state
  *           ISSUE: use passport constructors
  * baseURL: base URL for mounting OAuth login, callback URLs
- * sign: nacl.sign, with .keyPair(), .sign()
  */
-function makeGateway(app, passport, baseURL, sign) {
+exports.appFactory = appFactory;
+function appFactory({app, passport, baseURL}) {
     app.use(passport.initialize());
     passport.serializeUser((user, done) => done(null, user));
     passport.deserializeUser((obj, done) => done(null, obj));
@@ -32,24 +37,20 @@ function makeGateway(app, passport, baseURL, sign) {
 	discord: opts => new discord.Strategy(Object.assign({ scope: 'identity'}, opts), verify)
     };
 
-    return Object.freeze({ gateway, oauthClient });
+    return def({ gateway, oauthClient });
 
     function gateway(context) {
-	// ISSUE: clients don't revive when the server restarts.
-	const state = context.state;
+	let state = 'clients' in context.state ?
+		context.state : null /* state.X throws until init() */;
 
-	const b2h = bytes => new Buffer(bytes).toString('hex');
-	const h2b = hex => new Buffer(hex, 'hex');
-	const t2b = text => new Buffer(text);
+	return def({
+	    init, makeClient,
+	    clients: () => state.clients
+	});
 
 	function init() {
+	    state = context.state;
 	    state.clients = [];
-
-	    const key = sign.keyPair();
-	    state.publicKey = b2h(key.publicKey);
-	    state.secretKey = b2h(key.secretKey);
-
-	    console.log('@@gateway public key:', state.publicKey);
 	}
 
 	function makeClient(path, callbackPath, strategy, id, secret) {
@@ -59,25 +60,25 @@ function makeGateway(app, passport, baseURL, sign) {
 	    state.clients.push(it);
 	    return it;
 	}
-	const getClients = () => state.clients;
 
-	function publicKey() {
-	    return state.publicKey;
-	}
-
-	function signText(message) {
-	    const k = sign.keyPair.fromSecretKey(h2b(state.secretKey));
-	    return b2h(sign(t2b(message), k.secretKey));
-	}
-
-	return Object.freeze({ init, makeClient, getClients,
-			       publicKey, signText });
     }
 
     function oauthClient(context) {
-	const state = context.state;
+	let state; // state.X throws until init()
+	if ('strategy' in context.state) {
+	    state = context.state;
+	    use();
+	}
+
+	return def({
+	    init,
+	    path: () => state.path,
+	    strategy: () => state.strategy,
+	    clientId: () => state.id
+	});
 
 	function init(path, callbackPath, strategy, id, secret) {
+	    state = context.state;
 	    // console.log('client init:', { path, callbackPath, strategy, id });
 	    state.path = path;
 	    state.strategy = strategy;
@@ -92,9 +93,6 @@ function makeGateway(app, passport, baseURL, sign) {
 
 	function use() {
 	    const strategy = state.strategy;
-	    if (!strategy) {
-		return; // not yet initialized
-	    }
 	    const makeStrategy = strategies[strategy];
 	    if (!makeStrategy) {
 		throw new Error(`unknown strategy: ${strategy}`);
@@ -103,7 +101,7 @@ function makeGateway(app, passport, baseURL, sign) {
 	    opts.callbackURL = new URL(opts.callbackPath, baseURL).toString();
 
 	    passport.use(makeStrategy(opts, verify));
-	    DEBUG: console.log('opts:', opts);
+	    console.log('@@DEBUG: opts:', opts);
 
 	    app.get(state.path, passport.authenticate(strategy));
 
@@ -113,13 +111,8 @@ function makeGateway(app, passport, baseURL, sign) {
 		    (req, res) => {
 			res.redirect(`/user/${req.user.username}`);
 		    });
-
 	}
 
-	const getId = () => state.id;
-
-	use();
-	return Object.freeze({ init, getId });
     }
 
     function verify(accessToken, refreshToken, profile, done) {
@@ -132,13 +125,13 @@ function makeGateway(app, passport, baseURL, sign) {
 }
 
 
-function integrationTest(argv, {express, passport, sign}) {
+function integrationTest(argv, {express, passport}) {
     // ISSUE: refresh = require('passport-oauth2-refresh')
     const app = express();
-    const port = parseInt(argv[2]);
-    const base = `http://jambox:${port}`;
+    const host = argv[2], port = parseInt(argv[3]);
+    const baseURL = `http://${host}:${port}`;
 
-    const gwApp = makeGateway(app, passport, base, sign);
+    const gwApp = appFactory({app, passport, baseURL});
     function make(reviver, ...arg) {
 	console.log('make:', { reviver, arg });
 	const context = { state: {} };
@@ -149,9 +142,6 @@ function integrationTest(argv, {express, passport, sign}) {
     const gwContext = {state: {}, make};
     const gw = gwApp.gateway(gwContext);
     gw.init();
-
-    console.log('gateway public key:', gw.publicKey());
-    console.log('gateway public signature:', gw.signText('hello world'));
 
     const clgh = gw.makeClient(
 	'/auth/github/login', '/auth/github/callback', 'github',
@@ -164,17 +154,17 @@ function integrationTest(argv, {express, passport, sign}) {
 	'...',
 	'...');
 
+    console.log(`listening at ${baseURL}`);
     app.listen(port);
 }
 
 
 if (require.main == module) {
-    // Access ambient stuff only when invoked as main module.
+    // ocap: Import powerful references only when invoked as a main module.
     integrationTest(process.argv,
          {
 	     express: require('express'),
 	     // ISSUE: isolate global mutable state?
 	     passport: require('passport'),
-	     sign: require('tweetnacl').sign
 	 });
 }
