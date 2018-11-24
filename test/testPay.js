a9d5caf4292e2a5bbb9914e5e29be8b9434d986b
Dan Connolly <dckc@madmode.com>
2018-11-24 01:25:50 -0600

    testPay: BasicWallet.transfer accepts signature

--- test/testPay.js
+++ test/testPay.js
@@ -7,10 +7,14 @@ ISSUE: should be: all she knows about Bob is his eth address.
 */
 /* global require */
 
-const { RNode, b2h, h2b, RHOCore, makeProxy } = require('..');
+const { URL } = require('url');
+
+const { GPrivate } = require('../protobuf/RhoTypes.js');
+
+const { RNode, b2h, h2b, RHOCore, makeProxy, keyPair, blake2b256Hash } = require('..');
 const { link } = require('./assets');
 
-const { toJSData } = RHOCore;
+const { toJSData, rhol } = RHOCore;
 
 const aliceInit = link('./aliceBobWallets.rho');
 const defaultPayment = { from: '0x1', nonce: 0, phloPrice: 1, phloLimit: 100000 };
@@ -23,6 +27,14 @@ async function test({ rnode, clock, setTimeout }) {
 }
 
 
+const R = {
+  debug: new URL('rho:io:stderr'),
+  lookup: new URL('rho:registry:lookup'),
+  sys: new URL('rho:id:wdwc36f4ixa6xacck3ddepmgueum7zueuczgthcqp6771kdu8jogm8'),
+  BasicWallet: new URL('rho:id:3yicxut5xtx5tnmnneta7actof4yse3xangw4awzt8c8owqmddgyms'),
+};
+
+
 async function alicePaysBob(aliceWalletURI, { rnode, clock, _setTimeout }) {
   const aliceWallet = makeProxy(aliceWalletURI, { user, ...defaultPayment }, { rnode, clock });
 
@@ -30,6 +42,46 @@ async function alicePaysBob(aliceWalletURI, { rnode, clock, _setTimeout }) {
   console.log({ aliceBalance });
   const aliceNonce = await aliceWallet.getNonce();
   console.log({ aliceNonce });
+
+  const amount = 10;
+  const aliceKey = keyPair(h2b('f6664a95992958bbfeb7e6f50bbca2aa7bfd015aec79820caf362a3c874e9247'));
+  const dPmt = { user, timestamp: clock().valueOf(), ...defaultPayment };
+  const [retId] = await rnode.previewPrivateIds(dPmt, 1);
+  console.log('preview of unforgeable name needed for signing', retId);
+
+  const msg = RHOCore.toByteArray(RHOCore.fromJSData(
+    [aliceNonce + 1, amount, GPrivate.create({ id: retId })]
+  ));
+  console.log('transfer bytes to hash, sign', b2h(msg));
+  const sigHex = aliceKey.signBytesHex(blake2b256Hash(msg));
+  console.log('pk, sig:', aliceKey.publicKey(), sigHex);
+
+  const term = rhol`
+new retCh, statusCh, debug(${R.debug}), rl(${R.lookup}), wCh, bwCh, sysCh, revCh in {
+  rl!(${aliceWalletURI}, *wCh) |
+  for(aliceWallet <- wCh) {
+    debug!({"aliceWallet": *aliceWallet, "to hash, sign": [${aliceNonce + 1}, ${amount}, *retCh].toByteArray()}) |
+    // ISSUE: if you pass in bytes rather than hex for sig, nothing comes back
+    aliceWallet!("transfer", ${amount}, ${aliceNonce + 1}, ${sigHex}, *retCh, *statusCh) |
+    for(@status <- statusCh) {
+      debug!({"transfer status": status}) |
+      if (status == "Success") {
+          for(pmt <- retCh) {
+            debug!({"pmt": *pmt}) // ISSUE: purses can go out of scope and get lost forever
+          }
+      } else {
+        debug!("lose!")
+      }
+    }
+  }
+}
+
+  `;
+  console.log('code to pay:', dPmt.timestamp, term);
+  await rnode.doDeploy({ term, ...dPmt}, true);
+  const found = await rnode.listenForDataAtName({ ids: [{ id: retId }] });
+  const par = firstBlockData(found);
+  console.log('alice payment purse:', par);
 }
 
 
