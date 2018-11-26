4a99fda09bd9be9498978c9710401c7de564dca2
Dan Connolly <dckc@madmode.com>
2018-11-25 23:56:12 -0600

    proxy sendCall: support extra pre-declared names (IOU tests)
      - allow null method as well as ''

--- src/proxy.js
+++ src/proxy.js
@@ -7,7 +7,7 @@ import type { IRNode } from './rnodeAPI';
 
 type Message = {
   target: string,
-  method: string,
+  method: ?string,
   args: mixed[],
 };
 
@@ -28,12 +28,13 @@ type Message = {
  * @param opts.delay: an optional async function to call between sending
  *                    a call and listening for the response.
  * @param opts.unary: whether to use unary calling conventions.
+ * @param opts.predeclare: names to pre-declare after `return`
  */
 exports.makeProxy = makeProxy;
 function makeProxy(
   target /*: string */,
   deployData,
-  { rnode, clock, delay = null, unary = false },
+  { rnode, clock, predeclare = null, delay = null, unary = false },
 ) {
   const sendIt = msg => sendCall(
     msg, { timestamp: clock().valueOf(), ...deployData },
@@ -63,9 +64,9 @@ exports.sendCall = sendCall;
 async function sendCall(
   { target, method, args } /*: Message*/,
   deployData,
-  { rnode, delay = null, unary = false },
+  { rnode, predeclare = null, delay = null, unary = false },
 ) {
-  const term = callSource({ target, method, args }, unary);
+  const term = callSource({ target, method, args }, predeclare || [], unary);
   const [returnCh] = await rnode.previewPrivateChannels(deployData, 1);
   await rnode.doDeploy({ term, ...deployData }, true);
   if (delay) {
@@ -87,16 +88,16 @@ async function sendCall(
  * list arg on the rholang side.
  */
 exports.callSource = callSource;
-function callSource({ target, method, args } /*: Message*/, unary = false) {
+function callSource({ target, method, args } /*: Message*/, predeclare = [], unary = false) {
   return rhoCall({
     // ISSUE: assume target is injection-safe?
     target: `\`${target}\``,
-    method: method.length > 0 ? [rhol`${method}`] : [],
+    method: method && method.length > 0 ? [rhol`${method}`] : [],
     args: (
       unary
         ? [rhol`${args}`]
         : args.map(arg => rhol`${arg}`)),
-  });
+  }, predeclare);
 }
 
 
@@ -107,9 +108,13 @@ function callSource({ target, method, args } /*: Message*/, unary = false) {
  * @param m.target: a rholang URI expression: `rho:id:...`
  * @param m.method: [] or ["eat"]
  * @param m.args: a list of rholang terms
+ * @param predeclare: names to pre-declare in addition to `return`
+ *                    They are passed (deref's do processes) to target
+ *                    in reverse order.
  */
-function rhoCall({ target, method, args }) {
-  const pieces = [].concat(method, args, ['*return']);
+function rhoCall({ target, method, args }, predeclare = []) {
+  const newNames = ['return', ...predeclare.reverse()];
+  const pieces = [...method, ...args, [...predeclare, 'return'].map(n => `*${n}`)];
   /*
         // ISSUE: what comes back from the registry is
         // a tuple if insertSigned and just the thing if insertArbitrary.
@@ -117,8 +122,8 @@ function rhoCall({ target, method, args }) {
         for(@(_nonce, target) <- targetCh) {
    */
   const term = `
-      new return, targetCh, lookup(\`rho:registry:lookup\`), trace(\`rho:io:stderr\`) in {
-        trace!("hello from remote call.") |
+      new ${newNames.join(', ')}, targetCh, lookup(\`rho:registry:lookup\`), trace(\`rho:io:stderr\`) in {
+        trace!({"in remote call to": ${target}}) |
         lookup!(${target}, *targetCh) |
         for(target <- targetCh) {
           trace!({ "target": *target }) |
