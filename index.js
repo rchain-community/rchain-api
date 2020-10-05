3bf714cbfc483dc3dc5ab60771ee40d7a65ba6ff
Dan Connolly <dckc@madmode.com>
2020-10-04 21:47:18 -0500

    export types

--- index.js
+++ index.js
@@ -2,10 +2,26 @@
 
 // TODO: doctests?
 
+/**
+ * @typedef { import('./src/rnode').Observer } Observer
+ * @typedef { import('./src/rnode').Validator } Validator
+ * @typedef { import('./src/proxy').Account } Account
+ * @typedef { import('./src/rev-address').RevAccount } RevAccount
+ * @typedef { import('./src/rnode-openapi-schema').ExploratoryDeployResponse } ExploratoryDeployResponse
+ */
+
 import * as rhopmAll from './src/rhopm';
 
 export { RNode } from './src/rnode';
 export { RhoExpr } from './src/rho-expr';
+export { Base16, Base58 } from './src/codec';
 export { sign as signDeploy } from './src/deploySig';
+export {
+  getAddrFromEth,
+  getAddrFromPublicKey,
+  getAddrFromPrivateKey,
+  verifyRevAddr,
+  createRevAccount,
+} from './src/rev-address';
 export { makeAccount, makeConnection } from './src/proxy';
 export const rhopm = rhopmAll;
