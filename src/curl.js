77a25d7c00c039833cfaa078dfbcc92d7e431484
Dan Connolly <dckc@madmode.com>
2022-01-15 10:06:52 -0600

    style: @jessie-check for curl.js
     - add jessie.js eslint config
     - upgrade elint (why?)
     - use `makePromise` from `jessie.js` to avoid `new`
     - lock packages

--- src/curl.js
+++ src/curl.js
@@ -1,4 +1,7 @@
 // @ts-check
+// @jessie-check
+
+import { makePromise } from 'jessie.js';
 
 /**
  * @param {string} url
@@ -6,7 +9,7 @@
  * @returns {Promise<string>}
  */
 export function curl(url, { http }) {
-  return new Promise((resolve, reject) => {
+  return makePromise((resolve, reject) => {
     const req = http.get(url, response => {
       let str = '';
       // console.log('Response is ' + response.statusCode);
@@ -26,12 +29,12 @@ export function curl(url, { http }) {
 export function nodeFetch({ http }) {
   /** @type { typeof fetch } */
   function fetch(url, options = {}) {
-    return new Promise((resolve, reject) => {
+    return makePromise((resolve, reject) => {
       const { method = 'GET', body } = options;
-      if (typeof url !== 'string') { throw new Error('not implemented'); }
+      if (typeof url !== 'string') { throw Error('not implemented'); }
       const req = http.request(url, { method }, res => {
         res.setEncoding('utf8');
-        let content = ''
+        let content = '';
         res.on('data', data => { content += data; });
 
         res.on('end', () => {
